4b2daeb4c4a25929c36e64d2ca2ff4ff4222b25d
reset <app@jadendong.com>
2019-10-25 22:43:50 +0800

    init

---
+++ src/main/java/com/jadendong/happycoding/Test.java
@@ -0,0 +1,10 @@
+package com.jadendong.happycoding;
+
+/**
+ * @author jaden
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
